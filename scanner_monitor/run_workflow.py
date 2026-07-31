77a76666b422146ceac9a772d4b88b4b3776f2d1
PavanSaiNekkala <91945047+PavanSaiNekkala@users.noreply.github.com>
2026-07-31 18:47:54 +0530

    Update run_workflow.py

--- scanner_monitor/run_workflow.py
+++ scanner_monitor/run_workflow.py
@@ -15,7 +15,7 @@ from scanner_monitor.universe_loader import (
 # ==========================================================
 
 universe = pd.read_csv(
-    "scanner_monitor/backtest/data/ind_nifty500list.csv" #universe.csv"
+    "scanner_monitor/backtest/data/universe.csv" #ind_nifty500list.csv" 
 )
 
 _, sector_map, _ = get_buckets_and_sectors()
